5043690775687c0abeda316aafc7cf7642f60668
alanvdb <alanvdb.coder@gmail.com>
2025-05-19 04:26:18 +0200

    extends AbstractValidator

--- src/ComposerProjectNameValidator.php
+++ src/ComposerProjectNameValidator.php
@@ -3,23 +3,19 @@
 namespace AlanVdb\Composer;
 
 use AlanVdb\Validator\Definition\ValidatorInterface;
+use AlanVdb\Validator\AbstractValidator;
 
-class ComposerProjectNameValidator implements ValidatorInterface
+class ComposerProjectNameValidator
+    extends AbstractValidator
+    implements ValidatorInterface
 {
-    private string $errorMessage;
-
     public function __construct(string $errorMessage = 'The project name must be in the format vendor/package.')
     {
-        $this->errorMessage = $errorMessage;
+        parent::__construct($errorMessage);
     }
 
     public function isValid(mixed $value) : bool
     {
         return is_string($value) ? (bool) preg_match('/^[a-z0-9]([_.-]?[a-z0-9]+)*\/[a-z0-9]([_.-]?[a-z0-9]+)*$/', $value) : false;
-    }
-
-    public function getErrorMessage() : string
-    {
-        return $this->errorMessage;
     }
 }
